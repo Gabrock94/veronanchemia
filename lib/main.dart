34d9cdf6cc5aa3fdd45fa40b1e0c8e9d08d5542b
Giulio <gack94@gmail.com>
2022-09-30 16:09:27 +0200

    Accessibility fix
    Improved layout for screen readers accessibility.
    - changes to Text rendering. Now it renders elements paragraph-based. This allows screen reader users to skim trough the text
    - Added localization (Italian) for layout's elements (back buttons, navigation menu)
    - Navigation menu elements' converted into buttons

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 import 'package:better_player/better_player.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 final Uri _website = Uri.parse('https://www.veronanchemia.it/');
 final Uri _privacy = Uri.parse('https://www.veronanchemia.it/app/privacy.html');
@@ -132,6 +133,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Verona Anche Mia',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('it', ''), // English, no country code
+      ],
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
@@ -265,9 +274,12 @@ class _MyHomePageState extends State<MyHomePage> {
               child:
               Container(margin: EdgeInsets.only(left:30.0, top:20.0,right:30.0,bottom:20.0), child: Image.asset('assets/VAM_logo_transp.png'))
             ),
-            ListTile(
-              title: const Text('Verona Anche Mia'),
-              onTap: () {
+            TextButton(
+              child: const Text('Verona Anche Mia', style: TextStyle(color: Colors.black)),
+              style: ButtonStyle(
+                alignment: Alignment.centerLeft,
+              ),
+              onPressed: () {
                 Navigator.pop(context);
                 Navigator.push(
                     context,
@@ -275,9 +287,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 //
               },
             ),
-            ListTile(
-              title: const Text('Galleria Video'),
-              onTap: () {
+            TextButton(
+              child: const Text('Galleria Video', style: TextStyle(color: Colors.black)),
+              style: ButtonStyle(
+              alignment: Alignment.centerLeft,
+              ),
+              onPressed: () {
                 Navigator.pop(context);
                 Navigator.push(
                     context,
@@ -285,9 +300,12 @@ class _MyHomePageState extends State<MyHomePage> {
                 //
               },
             ),
-            ListTile(
-              title: const Text('Chi Siamo'),
-              onTap: () {
+            TextButton(
+              child: const Text('Chi siamo', style: TextStyle(color: Colors.black)),
+              style: ButtonStyle(
+                alignment: Alignment.centerLeft,
+              ),
+              onPressed: () {
                 Navigator.pop(context);
                 Navigator.push(
                     context,
@@ -295,16 +313,22 @@ class _MyHomePageState extends State<MyHomePage> {
                 //
               },
             ),
-            ListTile(
-              title: const Text('Privacy'),
-              onTap: () {
+            TextButton(
+              child: const Text('Privacy', style: TextStyle(color: Colors.black)),
+              style: ButtonStyle(
+                alignment: Alignment.centerLeft,
+              ),
+              onPressed: () {
                 _launchUrl(_privacy);
                 //
               },
             ),
-            ListTile(
-              title: const Text('Crediti'),
-              onTap: () {
+            TextButton(
+              child: const Text('Crediti', style: TextStyle(color: Colors.black)),
+              style: ButtonStyle(
+                alignment: Alignment.centerLeft,
+              ),
+              onPressed: () {
                 Navigator.pop(context);
                 Navigator.push(
                     context,
@@ -337,8 +361,9 @@ class tourPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(tour.title),
       ),
-      body: ListView(
-        children: [
+      body: SingleChildScrollView(
+            child: Column(
+              children: [
           Stack(children: [Image.asset(tour.image, fit: BoxFit.cover, width: double.infinity),
             Positioned.fill(
               child: DecoratedBox(
@@ -457,7 +482,7 @@ class tourPage extends StatelessWidget {
                 LocationListItem(
                   imageUrl: tour.video.preview,
                   name: tour.video.title,
-                  country: "",
+                  country: "Clicca qui per aprire l'approfondimento video",
                 ),
               ),
               Text("\nInizia il Tour", style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
@@ -478,7 +503,8 @@ class tourPage extends StatelessWidget {
           ),)
         ],
       )
-      );
+      )
+    );
   }
 }
 
@@ -487,13 +513,17 @@ class stepPage extends StatelessWidget {
   final Tour tour;
   final int stageIndex;
 
+
   @override
+
   Widget build(BuildContext context) {
+
     return Scaffold(
         appBar: AppBar(
           title: Text(tour.steps[stageIndex]['title']),
         ),
-        body: ListView(
+        body: SingleChildScrollView(
+          child: Column(
           children: [
             Stack(children: [Image.asset(tour.steps[stageIndex]['image'], fit: BoxFit.cover, width: double.infinity),
               Positioned.fill(
@@ -532,7 +562,11 @@ class stepPage extends StatelessWidget {
                 children: [
                   Padding(
                     padding: const EdgeInsets.only(bottom: 8),
-                    child: Text(tour.steps[stageIndex]['description'], style: const TextStyle(fontSize: 16)),
+                      child: Column(
+                        children:[
+                          for (String paragraph in tour.steps[stageIndex]['description'].split("\n")) Container(child:Text(paragraph, style: const TextStyle(fontSize: 16)), margin: EdgeInsets.only(bottom: 8),)
+                          ]
+                      ),
                   ),
             if(stageIndex < tour.steps.length - 1)
                   Padding(
@@ -590,32 +624,29 @@ class stepPage extends StatelessWidget {
             ),
           ],
         )
+    )
     );
   }
 }
 
 class aboutUs extends StatelessWidget {
   const aboutUs({super.key});
-
+  final String testopagina = "AMEntelibera è un’associazione culturale che nasce nel 2008 a San Bonifacio (Verona) e si occupa di promuovere stili di vita sostenibili attraverso esperienze in ambito educativo e di turismo di prossimità in senso inclusivo.\nIl progetto Verona Anche Mia è stato ideato dall’associazione con la volontà di dare un contributo per rendere Verona una città più accessibile e alla portata di tutti, realizzando percorsi destinati ai turisti con disabilità sensoriali ma anche ai residenti che vogliono conoscere la città con una modalità nuova, tecnologica e multisensoriale.\nAll’interno di AMEntelibera sono presenti figure di professioniste che hanno contribuito a vario titolo alla realizzazione del progetto: Manuela Uber, guida turistica con master di educatore esperto per le disabilità sensoriali e multifunzionali; Anna Corradini, accompagnatore turistico specializzata in organizzazione di eventi ed esperienze di viaggio; Tecla Soave, dottorata in scienze ambientali specializzata in educazione e comunicazione ambientale e valorizzazione del territorio.\nAl progetto hanno collaborato come partner istituzionali le associazioni G.O.L.D.VIS. e Genitori Tosti in Tutti i Posti insieme all’Istituto Istruzione Superiore Statale Copernico Pasoli di Verona.\nUn ringraziamento per il supporto all’interprete LIS Ornella Piazza.\nIl progetto è stato realizzato grazie al finanziamento ricevuto dall’Otto per Mille Valdese 2021.";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Chi Siamo'),
         ),
-        body: ListView(
-            children:[
-        Padding(
-        padding: const EdgeInsets.all(25),
-        child: Column(
+        body:   SingleChildScrollView(
+            padding: const EdgeInsets.all(25),
+            child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-            Text("AMEntelibera è un’associazione culturale che nasce nel 2008 a San Bonifacio (Verona) e si occupa di promuovere stili di vita sostenibili attraverso esperienze in ambito educativo e di turismo di prossimità in senso inclusivo.\n\nIl progetto Verona Anche Mia è stato ideato dall’associazione con la volontà di dare un contributo per rendere Verona una città più accessibile e alla portata di tutti, realizzando percorsi destinati ai turisti con disabilità sensoriali ma anche ai residenti che vogliono conoscere la città con una modalità nuova, tecnologica e multisensoriale.\n\nAll’interno di AMEntelibera sono presenti figure di professioniste che hanno contribuito a vario titolo alla realizzazione del progetto: Manuela Uber, guida turistica con master di educatore esperto per le disabilità sensoriali e multifunzionali; Anna Corradini, accompagnatore turistico specializzata in organizzazione di eventi ed esperienze di viaggio; Tecla Soave, dottorata in scienze ambientali specializzata in educazione e comunicazione ambientale e valorizzazione del territorio.\n\nAl progetto hanno collaborato come partner istituzionali le associazioni G.O.L.D.VIS. e Genitori Tosti in Tutti i Posti insieme all’Istituto Istruzione Superiore Statale Copernico Pasoli di Verona.\n\nUn ringraziamento per il supporto all’interprete LIS Ornella Piazza.\n\nIl progetto è stato realizzato grazie al finanziamento ricevuto dall’Otto per Mille Valdese 2021.", style: const TextStyle(fontSize: 16)),
+              for (String paragraph in testopagina.split("\n")) Container(child:Text(paragraph, style: const TextStyle(fontSize: 16)), margin: EdgeInsets.only(bottom: 24),)
             ]
         )
         ),
-            ]
-        )
     );
   }
 }
@@ -722,21 +753,19 @@ class credits extends StatelessWidget {
 
 class intro extends StatelessWidget {
   const intro({super.key});
-
+  final String testopagina = "Verona è spesso chiamata città della musica, dell’amore, dell’arte e grazie alle mura che nei secoli sono state costruite per difenderla, è stata nel 2000 riconosciuta come Patrimonio Unesco.\nDa oltre duemila anni la città è abitata e in ogni piazza e strada si trovano le tracce di questa lunga e interessante storia.\nVi suggeriamo quattro itinerari, della durata variabile tra un’ora e mezza e due ore e mezza. Per ciascuno troverete le tappe con tracciato gps, una descrizione, un video di approfondimento.\n“Verona for all”, il primo itinerario è il  più breve, vi guiderà nel centro della città ed è consigliato  a chi non conosce Verona o non ha molto tempo a disposizione, ma vuole vedere i luoghi imperdibili.\nIl secondo, “Verona for all plus”, di circa due ore, ricalca per alcuni tratti il primo, ma aggiunge tappe in luoghi limitrofi al centro.\nQuesti due itinerari sono accessibili anche a persone con disabilità motorie.\nLa terza proposta, “ Un assaggio di Veronetta”,  vi condurrà nel quartiere  a sinistra Adige, con una tappa  panoramica al colle di Castel San Pietro. Può essere abbinato ai primi due o fatto da solo, è una variazione interessante per chi è già stato in città, ha più tempo o desidera conoscere zone meno note.\nIl quarto percorso “Tre tesori senza tempo: Castelvecchio, San Zeno, Arsenale Austriaco” si svolge nella zona periferica del centro, più tranquilla, tra Castelvecchio e San Zeno e include una visita all’area dell’Arsenale austriaco. Questa ultima tappa è stata pensata e realizzata in collaborazione con una classe terza del Liceo Scientifico dell’Istituto Copernico Pasoli di Verona.\nA voi serve solo un po’ di curiosità! Benvenuti a Verona!";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Verona Anche Mia'),
         ),
-        body: ListView(
-            children:[
-              Padding(
+        body:
+        SingleChildScrollView(
                   padding: const EdgeInsets.all(25),
                   child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Verona è spesso chiamata città della musica, dell’amore, dell’arte e grazie alle mura che nei secoli sono state costruite per difenderla, è stata nel 2000 riconosciuta come Patrimonio Unesco.\nDa oltre duemila anni la città è abitata e in ogni piazza e strada si trovano le tracce di questa lunga e interessante storia.\nVi suggeriamo quattro itinerari, della durata variabile tra un’ora e mezza e due ore e mezza. Per ciascuno troverete le tappe con tracciato gps, una descrizione, un video di approfondimento.\n“Verona for all”, il primo itinerario è il  più breve, vi guiderà nel centro della città ed è consigliato  a chi non conosce Verona o non ha molto tempo a disposizione, ma vuole vedere i luoghi imperdibili.\nIl secondo, “Verona for all plus”, di circa due ore, ricalca per alcuni tratti il primo, ma aggiunge tappe in luoghi limitrofi al centro.\nQuesti due itinerari sono accessibili anche a persone con disabilità motorie.\nLa terza proposta, “ Un assaggio di Veronetta”,  vi condurrà nel quartiere  a sinistra Adige, con una tappa  panoramica al colle di Castel San Pietro. Può essere abbinato ai primi due o fatto da solo, è una variazione interessante per chi è già stato in città, ha più tempo o desidera conoscere zone meno note.\nIl quarto percorso “Tre tesori senza tempo: Castelvecchio, San Zeno, Arsenale Austriaco” si svolge nella zona periferica del centro, più tranquilla, tra Castelvecchio e San Zeno e include una visita all’area dell’Arsenale austriaco. Questa ultima tappa è stata pensata e realizzata in collaborazione con una classe terza del Liceo Scientifico dell’Istituto Copernico Pasoli di Verona.\nA voi serve solo un po’ di curiosità! Benvenuti a Verona!", style: const TextStyle(fontSize: 16)),
                         AspectRatio(
                           aspectRatio: 16 / 9,
                           child: BetterPlayer.network(
@@ -750,13 +779,12 @@ class intro extends StatelessWidget {
                         Padding(
                           padding: const EdgeInsets.only(top: 25),
                         ),
-                        Text("Verona è spesso chiamata città della musica, dell’amore, dell’arte e grazie alle mura che nei secoli sono state costruite per difenderla, è stata nel 2000 riconosciuta come Patrimonio Unesco.\nDa oltre duemila anni la città è abitata e in ogni piazza e strada si trovano le tracce di questa lunga e interessante storia.\nVi suggeriamo quattro itinerari, della durata variabile tra un’ora e mezza e due ore e mezza. Per ciascuno troverete le tappe con tracciato gps, una descrizione, un video di approfondimento.\n“Verona for all”, il primo itinerario è il  più breve, vi guiderà nel centro della città ed è consigliato  a chi non conosce Verona o non ha molto tempo a disposizione, ma vuole vedere i luoghi imperdibili.\nIl secondo, “Verona for all plus”, di circa due ore, ricalca per alcuni tratti il primo, ma aggiunge tappe in luoghi limitrofi al centro.\nQuesti due itinerari sono accessibili anche a persone con disabilità motorie.\nLa terza proposta, “ Un assaggio di Veronetta”,  vi condurrà nel quartiere  a sinistra Adige, con una tappa  panoramica al colle di Castel San Pietro. Può essere abbinato ai primi due o fatto da solo, è una variazione interessante per chi è già stato in città, ha più tempo o desidera conoscere zone meno note.\nIl quarto percorso “Tre tesori senza tempo: Castelvecchio, San Zeno, Arsenale Austriaco” si svolge nella zona periferica del centro, più tranquilla, tra Castelvecchio e San Zeno e include una visita all’area dell’Arsenale austriaco. Questa ultima tappa è stata pensata e realizzata in collaborazione con una classe terza del Liceo Scientifico dell’Istituto Copernico Pasoli di Verona.\nA voi serve solo un po’ di curiosità! Benvenuti a Verona!", style: const TextStyle(fontSize: 16)),
+                        for (String paragraph in testopagina.split("\n")) Container(child:Text(paragraph, style: const TextStyle(fontSize: 16)), margin: EdgeInsets.only(bottom: 24),)
+
                       ]
                   )
-              ),
-            ]
-        )
-    );
+              )
+        );
   }
 }
 
@@ -790,7 +818,7 @@ class videoList extends StatelessWidget {
                       LocationListItem(
                         imageUrl: videosList[index].preview,
                         name: videosList[index].title,
-                        country: "",
+                        country: "Clicca qui per aprire il video",
                       ),
                     );
                   },
@@ -814,14 +842,11 @@ class videoPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(video.title),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 25, right: 25),
-        child: ListView(
+      body:
+      SingleChildScrollView(
+        padding: const EdgeInsets.all(25),
+        child: Column(
           children: [
-            Padding(padding: const EdgeInsets.all(15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
                   Padding(
                     padding: const EdgeInsets.only(bottom: 12),
                     child: Text(video.title, style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
@@ -836,16 +861,14 @@ class videoPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top:12, bottom: 12),
-                    child: Text(video.description, style: const TextStyle(fontSize: 16)),
-                  ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 24),
+                ),
+                for (String paragraph in video.description.split("\n")) Container(child:Text(paragraph, style: const TextStyle(fontSize: 16)), margin: EdgeInsets.only(bottom: 24),)
+
                 ],
               ),
-            ),
-          ],
-        ),
-        ),
+            )
       );
   }
 }
